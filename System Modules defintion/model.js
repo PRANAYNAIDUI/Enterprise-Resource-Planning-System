82ef644a2d156e1c4da085740c59727ec5cbe83d
PRANAY_Gandhamaneni <106988539+PRANAYNAIDUI@users.noreply.github.com>
2025-07-19 14:53:47 +0530

    Create model.js

---
+++ System Modules defintion/model.js
@@ -0,0 +1,29 @@
+// modules/Inventory/product.model.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
